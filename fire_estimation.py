7452d26f6c52e11c8650f00a500f998866bb87a5
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-21 14:34:13 -0500

    Update fire_estimation.py

--- fire_estimation.py
+++ fire_estimation.py
@@ -14,5 +14,27 @@ plt.imshow(photo_data)
 photo_data.shape
 
 
+#set a bar of pixel all to zero (creating a black bar)
+photo_data[150:900, : ] = 0
+plt.figure(figsize=(10,10))
+plt.imshow(photo_data)
+
+
+#set a bar of pixel all to 255 (creating a white bar)
+photo_data[150:900, : ] = 255
+plt.figure(figsize=(10,10))
+plt.imshow(photo_data)
+
+#filtering out all low values in the 
+photo_data = imageio.imread('./Pictures/sd-3layers.jpg')
+print("Shape of photo_data:", photo_data.shape)
+low_value_filter = photo_data < 200
+print("Shape of low_value_filter:", low_value_filter.shape)
+
+plt.figure(figsize=(10,10))
+plt.imshow(photo_data)
+photo_data[low_value_filter] = 0
+plt.figure(figsize=(10,10))
+plt.imshow(photo_data
 
 
