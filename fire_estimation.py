55b42e6c05aa983a41cff9277377c59269f9dc84
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-21 14:12:01 -0500

    Update fire_estimation.py

--- fire_estimation.py
+++ fire_estimation.py
@@ -1,8 +1,13 @@
 #author: fatemeh
 
 import numpy as np
-
 import imageio 
 import matplotlib.pyplot as plt
+from skimage import data
+
+photo_data = imageio.imread('./Pictures/sd-3layers.jpg')
+type(photo_data)
+
+
 
 
