dc32eef3c08d74cc0b0d13750609577c80935351
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-21 15:19:01 -0500

    Update fire_estimation.py

--- fire_estimation.py
+++ fire_estimation.py
@@ -25,16 +25,24 @@ photo_data[150:900, : ] = 255
 plt.figure(figsize=(10,10))
 plt.imshow(photo_data)
 
-#filtering out all low values in the 
+#filtering out all low values in the image and making them all black (zero)
 photo_data = imageio.imread('./Pictures/sd-3layers.jpg')
 print("Shape of photo_data:", photo_data.shape)
 low_value_filter = photo_data < 200
 print("Shape of low_value_filter:", low_value_filter.shape)
-
 plt.figure(figsize=(10,10))
 plt.imshow(photo_data)
 photo_data[low_value_filter] = 0
 plt.figure(figsize=(10,10))
-plt.imshow(photo_data
+plt.imshow(photo_data)
+
+#creating a while diagonal line in the image
+rows_range = np.arange(len(photo_data))
+cols_range = rows_range
+print(type(rows_range))
+photo_data[rows_range, cols_range] = 255
+plt.figure(figsize=(15,15))
+plt.imshow(photo_data)
+           
 
 
