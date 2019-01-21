811b8bd8dafd7125095e7ea11ffc3638ad16fbdb
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-21 15:25:30 -0500

    Update fire_estimation.py

--- fire_estimation.py
+++ fire_estimation.py
@@ -4,6 +4,7 @@ import numpy as np
 import imageio 
 import matplotlib.pyplot as plt
 from skimage import data
+import random
 
 photo_data = imageio.imread('./Pictures/sd-3layers.jpg')
 type(photo_data)
@@ -47,11 +48,8 @@ plt.imshow(photo_data)
 #############creating a circle mask out of the image
 total_rows, total_cols, total_layers = photo_data.shape
 #print("photo_data = ", photo_data.shape)
-
 X, Y = np.ogrid[:total_rows, :total_cols]
 #print("X = ", X.shape, " and Y = ", Y.shape)
-
-
 center_row, center_col = total_rows / 2, total_cols / 2
 #print("center_row = ", center_row, "AND center_col = ", center_col)
 #print(X - center_row)
@@ -63,7 +61,6 @@ radius = (total_rows / 2)**2
 circular_mask = (dist_from_center > radius)
 #print(circular_mask)
 print(circular_mask[1500:1700,2000:2200])
-
 photo_data = imageio.imread('./Pictures/sd-3layers.jpg')
 photo_data[circular_mask] = 0
 plt.figure(figsize=(15,15))
