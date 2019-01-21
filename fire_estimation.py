260b042f8db86894d27d4922c3491538766fef79
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-21 14:13:00 -0500

    Update fire_estimation.py

--- fire_estimation.py
+++ fire_estimation.py
@@ -8,6 +8,11 @@ from skimage import data
 photo_data = imageio.imread('./Pictures/sd-3layers.jpg')
 type(photo_data)
 
+plt.figure(figsize=(15,15))
+plt.imshow(photo_data)
+
+photo_data.shape
+
 
 
 
